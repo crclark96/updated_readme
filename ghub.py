67536f1a8b8447a1d47eb98bebf971a60bc5f451
Collin Clark <crclark96@gmail.com>
2017-10-31 20:26:03 -0400

    fork, clone, remove, and delete a repository

--- ghub.py
+++ ghub.py
@@ -1,3 +1,4 @@
+from subprocess import call
 import json
 import github
 
@@ -5,6 +6,16 @@ with open("credentials.json","r") as file:
     creds = json.load(file)
 
 g = github.Github(creds["api_key"])
+user = g.get_user()
+all_repos = g.get_repos()
 
-for repo in g.get_repos():
-    print repo
+forks = []
+
+forks.append(user.create_fork(all_repos[0]))
+
+call(["git", "clone", forks[0].html_url])
+
+call(["rm", "-rf", forks[0].name])
+
+forks[0].delete()
+
